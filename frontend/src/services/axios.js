296eaff065443387030509d23b0280ab8acbd090
gaikwadsangameshwar <sangamgaikwad214@gmail.com>
2026-01-14 22:37:50 +0530

    axios changes

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://sangam-blog.vercel.app/api/v1",
+  baseURL: "https://sangam-blog.vercel.app",
   withCredentials: true,
 });
 export default axiosInstance;
