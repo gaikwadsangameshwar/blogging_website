0253bba7bd40885d37a8134f3234ca8f0aa08b03
gaikwadsangameshwar <sangamgaikwad214@gmail.com>
2026-01-14 22:49:50 +0530

    added the localhost

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app=express()
 config()
 
 app.use(cors({
-    origin:"https://personal-blog-rho-navy.vercel.app",
+    origin:["http://localhost:5173","https://personal-blog-rho-navy.vercel.app"],
     credentials:true
 }))
 
