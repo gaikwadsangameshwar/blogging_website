4a145a123263d4fee4a1c9c20ae7c097b6ccdc1d
gaikwadsangameshwar <sangamgaikwad214@gmail.com>
2026-01-16 00:35:07 +0530

    fix: update CORS origin to include the correct production URL

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ config()
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    ""
+    "https://my-personal-gamma.vercel.app/"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
